16f9ec5c71ca331a5f7d422de3dfb500ade8ed59
lucasportella <lucasportellaagu@gmail.com>
2021-05-28 19:35:52 -0300

    react components and route readt, time to build redux pieces

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = function() {
   return (
-    <></>
+    <>
+    <Link to="/Login">Ir para Login</Link>
+    </>
   )
 }
 
