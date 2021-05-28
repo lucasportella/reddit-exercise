dff2de98b31d5c7324f35ed3ac6c81158e150d4a
lucasportella <lucasportellaagu@gmail.com>
2021-05-28 19:17:41 -0300

    reddit project mounting pieces

---
+++ src/components/Cadastro.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Cadastro = function() {
+  return (
+    <></>
+  )
+}
+
+export default Cadastro;
---
+++ src/components/Clientes.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Clientes = function() {
+  return (
+    <></>
+  )
+}
+
+export default Clientes;
