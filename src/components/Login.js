67f54002e6cf979ec0f9205de11d526f16c2a7b1
lucasportella <lucasportellaagu@gmail.com>
2021-05-29 20:43:43 -0300

    login working without redux

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,32 +1,58 @@
-import React from 'react';
+import React from "react";
+import { Redirect } from "react-router-dom";
 
 class Login extends React.Component {
   constructor() {
     super();
     this.state = {
-      email: '',
-      password: '',
-    }
+      checked: false,
+      email: "",
+      password: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
   }
+
+  handleChange({ target: { name, value } }) {
+    console.log(this.state)
+    this.setState((oldState) => ({
+      ...oldState,
+      [name]: value,
+    }));
+  }
+
   render() {
+    const { checked } = this.state;
+    if (checked) {
+      return <Redirect to="/Clientes" />;
+    }
     return (
       <>
-      <form>
-        <fieldset>
-          <legend>Campo de Login</legend>
-          <label>
-            Email:
-            <input type="text"/>
-          </label>
-          <label>
-            Senha:
-            <input type="text"/>
-          </label>
-          <button type="button">Efetuar Login</button>
-        </fieldset>
-      </form>
+        <form>
+          <fieldset>
+            <legend>Campo de Login</legend>
+            <label>
+              Email:
+              <input
+                onChange={this.handleChange}
+                type="text"
+                name="email"
+                value={this.state.email}
+              />
+            </label>
+            <label>
+              Senha:
+              <input
+                onChange={this.handleChange}
+                type="password"
+                name="password"
+                value={this.state.password}
+              />
+            </label>
+            <button type="button">Efetuar Login</button>
+          </fieldset>
+        </form>
       </>
-    )
+    );
   }
 }
 
