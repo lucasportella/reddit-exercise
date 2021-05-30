d2a0fa94b7285bd78fff2b129af4e1925dedcc80
lucasportella <lucasportellaagu@gmail.com>
2021-05-30 10:10:37 -0300

    redirect smart boy working

--- src/components/Clientes.js
+++ src/components/Clientes.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Redirect } from "react-router-dom";
+import { connect } from 'react-redux';
 
-const Clientes = function() {
-  return (
-    <></>
-  )
+class Clientes extends React.Component {
+  render() {
+  const { isChecked } = this.props;
+  if (isChecked) {
+    return (
+      <><h1>Clientes cadastrados</h1></>
+    )
+  }
+  return <Redirect to="/Cadastro"/>
+
+}
 }
 
 export default Clientes;
+
+const mapStateToProps = (state) => ({
+  isChecked: state.loginVerifierReducer.checked
+})
+
+connect(mapStateToProps)(Clientes);
