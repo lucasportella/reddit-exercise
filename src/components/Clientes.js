022b1e7d173b1300fccbe734838fb92cccd7599e
lucasportella <lucasportellaagu@gmail.com>
2021-05-30 10:23:38 -0300

    correct bug from missing export connect

--- src/components/Clientes.js
+++ src/components/Clientes.js
@@ -4,21 +4,21 @@ import { connect } from 'react-redux';
 
 class Clientes extends React.Component {
   render() {
-  const { isChecked } = this.props;
+    const { isChecked } = this.props;
   if (isChecked) {
     return (
       <><h1>Clientes cadastrados</h1></>
     )
+  } else {
+    return <Redirect to="/Cadastro"/>
   }
-  return <Redirect to="/Cadastro"/>
+
 
 }
 }
-
-export default Clientes;
 
 const mapStateToProps = (state) => ({
   isChecked: state.loginVerifierReducer.checked
 })
 
-connect(mapStateToProps)(Clientes);
+export default connect(mapStateToProps)(Clientes);
